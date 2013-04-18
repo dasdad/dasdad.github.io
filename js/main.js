94922d2a028441c40933edf61f2a64acd19e0746
Leandro Facchinetti <leafac@gmail.com>
2013-04-18 20:30:45 -0300

    Align vertical center
    Finally!! \o/

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $(function(){
   $('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
+
+  var verticalCentralize = function(element, container) {
+    var $window    = $(window);
+
+    var $element   = $(element);
+    var $container = typeof container === 'undefined' ? $window : $(container);
+
+    var act = function() {
+      var newTop = ($container.height() - $element.height()) / 2;
+
+      if (newTop < 0 || $container.width() < 900) {
+        newTop = 0;
+      }
+
+      $element.css({
+        position: 'relative',
+        top:      newTop  + 'px'
+      });
+    };
+
+    act();
+    $(window).on('resize', act);
+  };
+
+  verticalCentralize($('div#container'));
 });

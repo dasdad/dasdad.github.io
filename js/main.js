faeb776f7baccae02d5014ced801c6b567e7419a
Leandro Facchinetti <leafac@gmail.com>
2013-04-18 15:04:25 -0300

    Basic HTML

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+$(function(){
+  //$('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
+  $('body').addClass('black-on-white');
+  //$('body').addClass('white-on-black');
+});

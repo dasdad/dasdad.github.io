23c94e69c18c68ce74d18dae28ab0834af9b75d0
Leandro Facchinetti <leafac@gmail.com>
2013-04-18 18:12:38 -0300

    Clean js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
 $(function(){
-  //$('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
-  $('body').addClass('black-on-white');
-  //$('body').addClass('white-on-black');
+  $('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
 });

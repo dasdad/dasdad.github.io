66bc94ee34a59e0b849bf9f0851f6fbed70d08ad
Leandro Facchinetti <leafac@gmail.com>
2013-04-18 19:27:48 -0300

    Revert "centralizando site"
    Bug caused by small windows:
    
    https://dl.dropboxusercontent.com/u/833400/videos/resize-bug.ogv
    
    This reverts commit 3810942e2c088912715f27dd6cd7964481e00e2d.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
 $(function(){
-	$('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
-
-	resize();
-
-  $(window).resize(function () {
-  	resize();
-  });
+  $('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
 });
-
-
-function resize(){
-	var ah = $("div#content").height();
-	var ph = $("html").height();
-	var mh = (ph - ah) / 2;
-	$("div#content").attr('style', 'margin-top:' + mh + "px !important");
-}

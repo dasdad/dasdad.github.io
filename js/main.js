3810942e2c088912715f27dd6cd7964481e00e2d
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-04-18 18:33:45 -0300

    centralizando site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(function(){
-  $('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
+	$('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
+
+	resize();
+
+  $(window).resize(function () {
+  	resize();
+  });
 });
+
+
+function resize(){
+	var ah = $("div#content").height();
+	var ph = $("html").height();
+	var mh = (ph - ah) / 2;
+	$("div#content").attr('style', 'margin-top:' + mh + "px !important");
+}

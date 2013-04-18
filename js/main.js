9e4818ca980f34b2620930f8c9408750ab45c31c
Leandro Facchinetti <leafac@gmail.com>
2013-04-18 19:22:09 -0300

    Improve style

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 $(function(){
-	$('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
+  $('body').addClass(Math.random() > 0.5 ? 'black-on-white' : 'white-on-black');
 
-	resize();
+  var centralize_content = function () {
+    var ah = $("div#content").height();
+    var ph = $("html").height();
+    var mh = (ph - ah) / 2;
+    $("div#content").attr('style', 'margin-top:' + mh + "px !important");
+  }
 
-  $(window).resize(function () {
-  	resize();
-  });
+  centralize_content();
+
+  $(window).on('resize', centralize_content);
 });
 
 
-function resize(){
-	var ah = $("div#content").height();
-	var ph = $("html").height();
-	var mh = (ph - ah) / 2;
-	$("div#content").attr('style', 'margin-top:' + mh + "px !important");
-}
